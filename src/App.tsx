2e6b91095393a0199a8f321f69a20d21012bf0a9
MananSharma12 <manan.sharma1205@gmail.com>
2025-09-04 02:30:07 +0530

    add wallet connection feature using wagmi

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,44 @@
 import './App.css'
+import { config } from "./config.ts";
+import { useAccount, useConnect, useDisconnect, WagmiProvider } from "wagmi";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient()
 
 function App() {
   return (
-    <>
-    </>
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <ConnectWallet />
+      </QueryClientProvider>
+    </WagmiProvider>
+  )
+}
+
+function ConnectWallet() {
+  const { connect, connectors } = useConnect()
+  const { address, isConnected } = useAccount()
+  const { disconnect } = useDisconnect()
+
+  if (isConnected) {
+    return (
+      <>
+        <div>Connected: {address}</div>
+        <button onClick={() => disconnect()}>Disconnect</button>
+      </>
+    )
+  }
+
+  return (
+    connectors.map(
+      connector =>
+        <button
+          key={connector.uid}
+          onClick={() => connect({ connector })}
+        >
+          Connect via {connector.name}
+        </button>
+    )
   )
 }
 
